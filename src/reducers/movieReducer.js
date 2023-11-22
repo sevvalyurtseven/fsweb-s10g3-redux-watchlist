0204305d2a006fff05532602770c014f3e144eed
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-22 18:59:58 +0300

    initialState olusturuldu

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,4 +1,10 @@
-const initialState = {};
+import { movies } from "../movies";
+
+const initialState = {
+    sira: 0,
+    movie: movies,
+    favMovies: [],
+};
 
 const rootReducer = (state = initialState, action) => {};
 

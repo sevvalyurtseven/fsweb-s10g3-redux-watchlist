baa35b2223e33164158f1f341b7f9078876ee1a7
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-23 03:02:13 +0300

    App crashed hatasi cozuldu.

--- src/App.js
+++ src/App.js
@@ -2,39 +2,53 @@ import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
 import { useDispatch, useSelector } from "react-redux";
-import { addFavorite, firstMovie, nextMovie, prevMovie } from "./actions/actions";
+import {
+  addFavorite,
+  firstMovie,
+  nextMovie,
+  prevMovie,
+} from "./actions/actions";
 
 function App() {
   //const [sira, setSira] = useState(0);
-  const sira = useSelector(store => store.sira);
-  const favMovies = useSelector(store => store.favMovies);
+  const sira = useSelector((store) => store.sira);
+  const favMovies = useSelector((store) => store.favMovies);
 
-  const movies = useSelector((store) => store.favMovies);
+  const movies = useSelector((store) => store.movies);
 
   const dispatch = useDispatch();
 
   function handleNext() {
-    dispatch(nextMovie())
+    dispatch(nextMovie());
   }
 
   function handlePrev() {
-    dispatch(prevMovie())
+    dispatch(prevMovie());
   }
 
   function handleFirstMovie() {
-    dispatch(firstMovie())
+    dispatch(firstMovie());
   }
   const handleAddFavorite = () => {
-    dispatch(addFavorite(movies[sira]))
-  }
+    dispatch(addFavorite(movies[sira]));
+  };
 
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
@@ -43,25 +57,35 @@ function App() {
           <Movie sira={sira} />
 
           <div className="flex gap-3 justify-end py-3">
+            {sira > 0 && (
+              <>
+                <button
+                  onClick={handleFirstMovie}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Başa Dön
+                </button>
+                <button
+                  onClick={handlePrev}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Önceki
+                </button>
+              </>
+            )}
+
             <button
-              onClick={handleFirstMovie}
-              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
-            >
-              Başa Dön
-            </button>
-            <button
-              onClick={handlePrev}
-              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
-            >
-              Önceki
-            </button>
-            <button
+              disabled={movies.length - 1 === sira}
               onClick={handleNext}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Sıradaki
             </button>
-            <button onClick ={handleAddFavorite} className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
+
+            <button
+              onClick={handleAddFavorite}
+              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
+            >
               Listeme ekle
             </button>
           </div>
